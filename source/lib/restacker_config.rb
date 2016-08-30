4b895573a1875711084d0f62fd66abeb8ecb6ce1
Peter Benjamin <peter_benjamin@intuit.com>
2016-08-29 21:50:23 -0700

    Refactor restacker_config.rb

--- source/lib/restacker_config.rb
+++ source/lib/restacker_config.rb
@@ -1,7 +1,30 @@
 class RestackerConfig
   def self.load_config(plane)
+    plane = get_plane if plane.nil?
+    config = find_config
+    # puts "Inside 'self.load_config'"
+    # puts "Config: #{config}"
+    # puts "Plane: #{plane}"
+    if config[plane].nil?
+      puts "Plane not found (#{plane}). Please see #{CONFIG_FILE}."
+      exit
+    end
+    config[plane]
+  end
+
+  def self.get_plane(options)
+    if options[:location]
+      plane = options[:location]
+    else
+      plane = find_default_plane()
+    end
+    plane.to_sym
+  end
+
+  private
+  def self.find_config
+    Dir.mkdir(CONFIG_DIR) unless Dir.exist?(CONFIG_DIR)
     begin
-      Dir.mkdir(CONFIG_DIR) unless Dir.exist?(CONFIG_DIR)
       if File.exist?(CONFIG_FILE)
         config = YAML.load_file(CONFIG_FILE)
       else
@@ -9,15 +32,21 @@ class RestackerConfig
         File.open(CONFIG_FILE, 'w') { |f| f.write config.to_yaml }
       end
     rescue Psych::SyntaxError
-      raise "Improperly formatted YAML file: #{CONFIG_FILE}, please ensure it is properly formatted"
+      raise "Improperly formatted YAML file: #{CONFIG_FILE}."
     rescue => e
       puts e.message
     end
+    config
+  end
 
-    if config[plane].nil?
-      puts "Plane not found (#{plane}), please see #{CONFIG_FILE}"
-      exit
+  def self.find_default_plane
+    # puts "Inside 'self.find_default_plane'"
+    config = find_config()
+    # puts "config: #{config}"
+    if config[:default][:label]
+      config[:default][:label].to_sym
+    else
+      raise "Location was not provided and no default location was found in #{CONFIG_FILE}."
     end
-    config[plane]
   end
 end
