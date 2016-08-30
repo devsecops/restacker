53b34dc30809ecc44b582c49e81de88bffe95022
Peter Benjamin <peter_benjamin@intuit.com>
2016-08-29 21:49:30 -0700

    Refactor base_stacker.rb

--- source/lib/base_stacker.rb
+++ source/lib/base_stacker.rb
@@ -1,16 +1,15 @@
 require 'aws-sdk'
-require 'yaml'
 require 'json'
+require 'yaml'
+require 'rainbow'
 
-VERSION = '0.0.11'
+VERSION = '0.1.0'
 CONFIG_DIR="#{ENV['HOME']}/.restacker"
 CONFIG_FILE="#{CONFIG_DIR}/restacker.yml"
 SAMPLE_FILE = "#{__dir__}/../restacker-sample.yml"
 
 # needed here (after config_dir and defaults_file)
 require_relative 'auth'
-
-DEFAULT_PLANE = :ksp
 
 CREATE_COMPLETE = 'CREATE_COMPLETE'
 CREATE_IN_PROGRESS = 'CREATE_IN_PROGRESS'
@@ -19,12 +18,14 @@ DELETE_COMPLETE = 'DELETE_COMPLETE'
 
 class BaseStacker
   def initialize(options)
-    @plane = options[:location] ? options[:location].to_sym : DEFAULT_PLANE
-    config = RestackerConfig.load_config(@plane)
+    location = RestackerConfig.get_plane(options)
+    config = RestackerConfig.load_config(location)
+
     # use default region if not passed in from cli
     config[:region] = options[:region] if options[:region]
     options[:region] = config[:region] unless options[:region]
-    @cf, @creds = Auth.login(options, config, @plane)
+
+    @cf, @creds = Auth.login(options, config, location)
     @options = options
   end
 end
